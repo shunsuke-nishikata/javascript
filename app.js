e768e1a4c9c6487fa6168e07ad01b9e32228e545
shunsuke-nishikata <1037shun@gmail.com>
2020-09-20 23:20:41 +0900

    javascript_training

---
+++ app.js
@@ -0,0 +1,21 @@
+// var max = 100000
+// var num = 1
+// var count = 0
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だったのは' + count + '回です');
+
+// for (初期値; 条件; 増減値;)
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
